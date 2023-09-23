cf3c0a70a5e7a1e8be2e7d14c176201bd9be1338
Kian Yew Ngieng <kianyewngieng@Kians-MacBook-Air.local>
2023-09-23 17:14:03 +0800

    add recursion for reversing

--- algorithms/recursion/reverse_elements_recursion.py
+++ algorithms/recursion/reverse_elements_recursion.py
@@ -34,5 +34,8 @@ def reverse(lst, start, stop):
         lst[stop] = to_swap
         reverse(lst, start+1, stop-1)
     return lst
+
+# time complexity O(N)
+# space complexity N/2 +1 recursive calls = O(N)
 lst = [1,2,3,4,5]
 reverse(lst, 0, len(lst)-1)
